27d701108f065d44d7ffc12585987b564ceab3c6
Gusto Workspace <gusto.workspace@gmail.com>
2026-09-25 10:13:19 +0200

    add favicon

--- client/src/pages/_document.page.js
+++ client/src/pages/_document.page.js
@@ -3,7 +3,11 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="fr">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
